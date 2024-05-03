071e175916a9bcce706d9d7d285ff0feecfc9ca4
Silvia <silviatc1993@gmail.com>
2024-05-03 12:15:38 +0200

    added new info component for landing page

---
+++ src/components/common/InfoSection.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const InfoSection = () => {
+  return (
+    <section>
+      <h2>¿Qué es FurMed?</h2>
+      <p>ingo</p>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </section>
+  );
+};
+
+export default InfoSection;
